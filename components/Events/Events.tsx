90de4095243aaa9ed5fae00fdfc9884241d95c3b
Manoj Kumar <manojkumararumainathan@gmail.com>
2025-05-20 22:03:02 +0530

    Updation :Events image webp pics

--- components/Events/Events.tsx
+++ components/Events/Events.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import Image from "next/image";
-import WhiteImg1 from "@/assets/white_bg/white1.png"
-import WhiteImg2 from "@/assets/white_bg/white2.jpg"
-import WhiteImg3 from "@/assets/white_bg/white3.jpg"
-import WhiteImg4 from "@/assets/white_bg/white4.jpg"
+import WhiteImg1 from "@/assets/Events/white1.webp"
+import WhiteImg2 from "@/assets/Events/white2.webp"
+import WhiteImg3 from "@/assets/Events/white3.webp"
+import WhiteImg4 from "@/assets/Events/white4.webp"
 
 const imageList = [
-  // WhiteImg1,
-  // WhiteImg2,
-  // WhiteImg3,
-  // WhiteImg4,
-""
+  WhiteImg1,
+  WhiteImg2,
+  WhiteImg3,
+  WhiteImg4,
 ];
 
 const WhiteBG = () => {
   return (
-    <div id="events" className="w-full min-h-auto  bg-white py-10 px-4 mt-10">
+    <div id="events" className="w-full min-h-auto  bg-white py-10 px-8 mt-10">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {imageList.map((src, index) => (
           <div
